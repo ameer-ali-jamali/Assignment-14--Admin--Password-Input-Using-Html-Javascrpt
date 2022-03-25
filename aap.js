cd0b0be0e611c745251fb5c0c9698d540b648aca
ameer-ali-jamali <ameer.ali.jamali886@gmail.com>
2022-03-25 08:01:30 +0500

    success-name-changed

--- aap.js
+++ aap.js
@@ -1,7 +1,7 @@
 var Password=prompt("Enter Administrator Password")
 // confirm("R You Sure")
 if(Password=="admin"){
-    document.write(`<h1 class="text-center success">Success login </h1>`)
+    document.write(`<h1 class="text-center success"> Logged in</h1>`)
 }
 else{
     document.write('<h1 class="text-center fail">Login failed</h1>')
